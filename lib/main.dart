8a782dc4dbe24f96d08f59e4061c7d3a929ee248
AbrorKhurramov <40171345+AbrorKhurramov@users.noreply.github.com>
2021-12-11 13:45:52 +0500

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,35 @@
-import 'package:example_app/controllers/product_controller.dart';
-import 'package:example_app/views/product_tile.dart';
+import 'package:example_app/bindings/main_bindings.dart';
+import 'package:example_app/controller/home_controller.dart';
+import 'package:example_app/provider/api_client.dart';
+import 'package:example_app/views/character/characters_page.dart';
+import 'package:example_app/views/episode/episode_page.dart';
+import 'package:example_app/views/location/location_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:get/get.dart';
 import 'package:get/instance_manager.dart';
+import 'package:get_storage/get_storage.dart';
 
-void main() {
+void main() async{
+  await GetStorage.init();
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
 }
+
+void onStart() {
+  WidgetsFlutterBinding.ensureInitialized();
+}
+
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "FLutter Example",
+    return GetMaterialApp(
+      navigatorKey: ApiClient.alice.getNavigatorKey(),
+      title: "Flutter Example",
+      initialBinding: MainBindings(),
       theme:
           new ThemeData(visualDensity: VisualDensity.adaptivePlatformDensity),
       debugShowCheckedModeBanner: false,
@@ -24,68 +38,21 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  final ProductController productController = Get.put(ProductController());
 
-  MyHomePage({Key? key}) : super(key: key);
+
+class MyHomePage extends GetView<HomeController> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        elevation: 0,
-        leading: Icon(
-          Icons.arrow_back_ios,
+    return GetBuilder<HomeController>(
+      builder:(controller)=> Scaffold(
+        body: PageView(
+          children: [
+            CharactersPage(),
+            LocationPage(),
+            EpisodePage()
+          ],
         ),
-        actions: [
-          IconButton(
-            icon: Icon(
-              Icons.shopping_cart,
-            ),
-            onPressed: () {},
-          )
-        ],
-      ),
-      body: Column(
-        children: [
-          Padding(
-            padding: EdgeInsets.all(16),
-            child: Row(
-              children: [
-                Expanded(
-                    child: Text(
-                  "ShopX",
-                  style: TextStyle(fontSize: 32, fontWeight: FontWeight.w900),
-                )),
-                IconButton(
-                  icon: Icon(Icons.view_list_rounded),
-                  onPressed: () {},
-                ),
-                IconButton(
-                  icon: Icon(Icons.grid_view),
-                  onPressed: () {},
-                ),
-              ],
-            ),
-          ),
-          Expanded(
-            child: Obx(() {
-              if(productController.isLoading.value)
-                return Center(child: CircularProgressIndicator());
-              else
-              return StaggeredGridView.countBuilder(
-                crossAxisCount: 2,
-                itemCount: productController.productList.length,
-                mainAxisSpacing: 16,
-                crossAxisSpacing: 16,
-                itemBuilder: (context, index) {
-                  return ProductTile(productController.productList[index]);
-                },
-                staggeredTileBuilder: (index) => StaggeredTile.fit(1),
-              );
-            }),
-          )
-        ],
       ),
     );
   }
